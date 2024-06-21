3a6d81ffd3f5959e29a30516b5815502d4fdb672
Daniel Miladinov <daniel.miladinov@gmail.com>
2024-06-21 09:58:49 -0400

    Make Initial Test Pass
    Signed-off-by: Daniel Miladinov <daniel.miladinov@gmail.com>

--- src/danielmiladinov/task_management_system.clj
+++ src/danielmiladinov/task_management_system.clj
@@ -1,6 +1,17 @@
 (ns danielmiladinov.task-management-system
   (:gen-class))
 
+(def tasks (atom []))
+
+(defn by-priority-and-status
+  "Return a collection of the tasks in the system in descending priority and status order"
+  []
+  (sort-by (juxt (comp {:high 1 :medium 2 :low 3}
+                       :priority)
+                 (comp {:in-progress 1 :pending 2 :completed 3}
+                       :status))
+           @tasks))
+
 (defn greet
   "Callable entry point to the application."
   [data]
--- test/danielmiladinov/task_management_system_test.clj
+++ test/danielmiladinov/task_management_system_test.clj
@@ -1,25 +1,37 @@
 (ns danielmiladinov.task-management-system-test
   (:require [burpless :refer [run-cucumber step]]
+            [clojure.string :as str]
             [clojure.test :refer [deftest is]]
             [danielmiladinov.task-management-system :as tms])
-  (:import (io.cucumber.datatable DataTable)
-           (io.cucumber.docstring DocString)
-           (io.cucumber.java PendingException)))
+  (:import (clojure.lang IObj)
+           (io.cucumber.datatable DataTable)))
+
+(def to-keyword
+  "Turns e.g. “In Progress” into :in-progress"
+  (comp keyword
+        #(str/replace % #"\s" "-")
+        str/lower-case))
 
 (def steps
   [(step :Given "I have the following tasks:"
          ^:datatable
          (fn [state ^DataTable dataTable]
-           (throw (PendingException.))))
+           (reset! tms/tasks (->> (.asMaps dataTable)
+                                  (map (fn [m]
+                                         (-> (update-keys m to-keyword)
+                                             (update :status to-keyword)
+                                             (update :priority to-keyword))))))
+           state))
 
    (step :When "I list my tasks by priority and status"
          (fn [state]
-           (throw (PendingException.))))
+           (assoc state :actual-tasks (tms/by-priority-and-status))))
 
    (step :Then "I should see the following tasks:"
-         ^:docstring
-         (fn [state ^DocString docString]
-           (throw (PendingException.))))])
+         ^{:docstring IObj}
+         (fn [{:keys [actual-tasks] :as state} ^IObj expected-tasks]
+           (assert (= expected-tasks actual-tasks))
+           state))])
 
 (deftest task-management-system-feature
   (is (zero? (run-cucumber "test-resources/task-management-system.feature" steps))))
